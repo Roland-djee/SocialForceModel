6a503a603a1c506b36ab4b2e87d4a86a9e7d2cd9
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-07 14:07:50 +0100

    define class for buildings

---
+++ buildingParameters/buildingType.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import numpy as np
+
+class buildingType:
+    '''Defines the type of building and packages the dimensions'''
+    def __init__(self, buildingType, coordinates):
+        self.type = buildingType
+        if (buildingType == 'wall'):
+            self.coordinates = np.array(coordinates)
+            self.color = 'black'
+        elif (buildingType == 'office'):
+            self.coordinates = np.array(coordinates)
+            self.color = 'grey' 
+
+        
+        
+            
