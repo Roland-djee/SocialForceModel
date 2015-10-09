04e5c8f0edb4e4b5abd55e45df0f6a22c2d0c8a3
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-09 12:01:00 +0100

    Comment some stuff

--- spawners/buildingSpawner.py
+++ spawners/buildingSpawner.py
@@ -23,17 +23,17 @@ def spawnWalls():
 		walls[i] = buildingType('wall', np.array([wallsCoordinates[2*i], wallsCoordinates[2*i+1]]))
 	return walls
 	
-# walls, buildings = spawnEnvironment()
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# for i in range(nbWalls):
-# 	plt.plot([walls[i].coordinates[0][0],walls[i].coordinates[1][0]], [walls[i].coordinates[0][1],walls[i].coordinates[1][1]], walls[i].color, linewidth=5)
+walls, buildings = spawnEnvironment()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+for i in range(nbWalls):
+	plt.plot([walls[i].coordinates[0][0],walls[i].coordinates[1][0]], [walls[i].coordinates[0][1],walls[i].coordinates[1][1]], walls[i].color, linewidth=5)
 
-# for i in range(nbBuildings):
-# 	build1 = matplotlib.patches.Rectangle((buildings[i].coordinates[0][0],buildings[i].coordinates[0][1]), 20, 20, color=buildings[i].color)
-# 	ax.add_patch(build1)
+for i in range(nbBuildings):
+	build1 = matplotlib.patches.Rectangle((buildings[i].coordinates[0][0],buildings[i].coordinates[0][1]), 20, 20, color=buildings[i].color)
+	ax.add_patch(build1)
 
-# plt.xlim([-worldLength, worldLength])
-# plt.ylim([-worldWidth, worldWidth])
-# plt.show()
+plt.xlim([-worldLength, worldLength])
+plt.ylim([-worldWidth, worldWidth])
+plt.show()
 
