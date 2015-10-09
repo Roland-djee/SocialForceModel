20cbb031b3f0463c60b7e6b7dd9683bc916c1658
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-09 17:25:59 +0100

    set test for pedestrianType class. Checks over all input elements

---
+++ test/testPedestrianType.py
@@ -0,0 +1,39 @@
+'''
+Created on 9 Oct 2015
+
+@author: RolandGuichard
+'''
+import unittest
+
+from pedestrianParameters.pedestrianType import *
+
+import numpy as np
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testPedestrianTypeWithPedestrian(self):
+        pedestrian = 'pedestrian'
+        position = np.array([2., 4., 6.]) 
+        velocity = np.array([1., 3., 5.]) 
+        target   = np.array([10., 15., 20.]) 
+        returnedPedestrian = pedestrianType(pedestrian, position, velocity, target)
+        errorMessage = "Wrong input value for pedestrian"
+        self.assertEqual(returnedPedestrian.type, pedestrian, errorMessage)
+        errorMessage = "Wrong input value for position"
+        self.assertEqual(returnedPedestrian.position.all(), position.all(), errorMessage)
+        errorMessage = "Wrong input value for velocity"
+        self.assertEqual(returnedPedestrian.velocity.all(), velocity.all(), errorMessage)
+        errorMessage = "Wrong input value for target"
+        self.assertEqual(returnedPedestrian.target.all(), target.all(), errorMessage)
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
