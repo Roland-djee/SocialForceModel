8dfb18db561eb5a743b56768b07f1969b4ea44fd
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-09 12:02:29 +0100

    set propagateParameters to git tracker.

---
+++ behaviours/propagateParameters.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import numpy as np
+
+# set time-propagation parameters 
+dt     = 0.001   # Time spacing [s]
+tmax   = 10.     # Maximum propagation time [s]
+t      = 0.      # Initial timing [s]
+time   = np.linspace(0., tmax, int(tmax/dt) + 1) # time grid
