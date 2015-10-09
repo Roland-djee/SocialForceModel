de177acc0e16eec9a5e5cef8dae9eec31def9a55
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-09 17:24:59 +0100

    simplify buildingType

--- buildingParameters/buildingType.py
+++ buildingParameters/buildingType.py
@@ -10,18 +10,12 @@ class buildingType:
             raise 'Wrong number of coordinates in "wallsCoordinates"'
         self.type = buildingType
         if (buildingType == 'wall'):
-            self.x1 = wallsCoordinates[0][0]
-            self.y1 = wallsCoordinates[0][1]
-            self.x2 = wallsCoordinates[1][0]
-            self.y2 = wallsCoordinates[1][1]
+            self.endPoint1 = wallsCoordinates[0]
+            self.endPoint2 = wallsCoordinates[1]
             self.color = 'black'
         elif (buildingType == 'office'):
-            self.corner1x = wallsCoordinates[0][0]
-            self.corner1y = wallsCoordinates[0][1]
-            self.corner2x = wallsCoordinates[1][0]
-            self.corner2y = wallsCoordinates[1][1]
-            self.corner3x = wallsCoordinates[2][0]
-            self.corner3y = wallsCoordinates[2][1]
-            self.corner4x = wallsCoordinates[3][0]
-            self.corner4y = wallsCoordinates[3][1]
+            self.corner1 = wallsCoordinates[0]  
+            self.corner2 = wallsCoordinates[1]
+            self.corner3 = wallsCoordinates[2]
+            self.corner4 = wallsCoordinates[3]
             self.color = 'grey' 
