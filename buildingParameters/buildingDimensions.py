a26a47591a1dc488a17acd7819425a17de90fe08
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-12 16:12:41 +0100

    Modified for simple test case.

--- buildingParameters/buildingDimensions.py
+++ buildingParameters/buildingDimensions.py
@@ -5,16 +5,14 @@ import numpy as np
 U0 = 10.        # Wall repulsive potential amplitude [m^2 s^2]
 R0 = 0.2        # Wall damping parameter [m]
 
-nbWalls = 2
+nbWalls = 1
 
 corner1 = np.array([-5., -2., 0.])
 corner2 = np.array([2., 5., 0.])
-corner3 = np.array([10., 10., 0.])
-corner4 = np.array([-10., 10., 0.])
 
-wallsCoordinates = np.array([corner1, corner2, corner3, corner4])
+wallCoordinates = np.array([corner1, corner2])
 
-nbBuildings = 2
+nbBuildings = 1
 
 # building coordinates from the down left corner and in clockwise direction
 
@@ -28,4 +26,4 @@ corner6 = np.array([-40., 40., 0.])
 corner7 = np.array([-20., 40., 0.])
 corner8 = np.array([-20., 20., 0.])
 
-buildingCoordinates = np.array([corner1, corner2, corner3, corner4, corner5, corner6, corner7, corner8])
+buildingCoordinates = np.array([corner1, corner2, corner3, corner4])
