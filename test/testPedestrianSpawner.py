37d5d6219116281348d1fec299b4cc71f6a92dc6
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-12 10:30:03 +0100

    Add test for pedestrian Spawner

---
+++ test/testPedestrianSpawner.py
@@ -0,0 +1,38 @@
+'''
+Created on 12 Oct 2015
+
+@author: RolandGuichard
+'''
+import unittest
+
+import matplotlib.pyplot as plt
+
+from pedestrianParameters.pedestrianSettings import *
+from spawners.pedestrianSpawner import *
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testPedestrianSpawnerWithRandomlyPlacedPedestrians(self):
+        pedestrian = spawnRandomPedestrians()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        for i in range(nbPedestrians):
+            plt.plot([pedestrian[i].position[0]],[pedestrian[i].position[1]], 'bo')
+        plt.xlim([-worldLength, worldLength])
+        plt.ylim([-worldWidth, worldWidth])
+        plt.show()
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
