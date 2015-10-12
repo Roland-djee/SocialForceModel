5ca4851fb67c112dea07bbb7843b67ba06f6454e
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-12 16:13:31 +0100

    Rename variables

--- buildingParameters/buildingType.py
+++ buildingParameters/buildingType.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 import numpy as np
-from buildingDimensions import wallsCoordinates
+from buildingDimensions import wallCoordinates
 
 class buildingType:
     '''Defines the type of building and packages the dimensions'''
     
-    def __init__(self, buildingType, *wallsCoordinates):
-        if len(wallsCoordinates) !=2 and len(wallsCoordinates) !=4:
+    def __init__(self, buildingType, *wallCoordinates):
+        if len(wallCoordinates) !=2 and len(wallCoordinates) !=4:
             raise 'Wrong number of coordinates in "wallsCoordinates"'
         self.type = buildingType
         if (buildingType == 'wall'):
-            self.endPoint1 = wallsCoordinates[0]
-            self.endPoint2 = wallsCoordinates[1]
+            self.endPoint1 = wallCoordinates[0]
+            self.endPoint2 = wallCoordinates[1]
             self.color = 'black'
         elif (buildingType == 'office'):
-            self.corner1 = wallsCoordinates[0]  
-            self.corner2 = wallsCoordinates[1]
-            self.corner3 = wallsCoordinates[2]
-            self.corner4 = wallsCoordinates[3]
+            self.corner1 = wallCoordinates[0]  
+            self.corner2 = wallCoordinates[1]
+            self.corner3 = wallCoordinates[2]
+            self.corner4 = wallCoordinates[3]
             self.color = 'grey' 
