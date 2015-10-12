9446eecbdf52eea3c09d1205707793a144d42caf
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-12 18:11:42 +0100

    Add force amplitude as a parameter

--- pedestrianParameters/pedestrianSettings.py
+++ pedestrianParameters/pedestrianSettings.py
@@ -3,6 +3,7 @@
 nbPedestrians = 10
 
 # set parameters for pedestrian-pedestrian repulsive forces from [Helbing & Molnar Phys. Rev. E 51, 4282 (1995)]
-v0    = 1.5     # Average desired speed [m/s]
-V12   = 2.1     # Repulsive potential amplitude [m^2 s^-2]
-sigma  = 0.3    # Potential damping parameter [m]
+v0        = 1.5         # Average desired speed [m/s]
+V12       = 2.1         # Repulsive potential amplitude [m^2 s^-2]
+sigma     = 0.3         # Potential damping parameter [m]
+amplitude = V12 / sigma # Pedestrian-pedestrian repulsive force amplitude [m^-1 s^-2]
