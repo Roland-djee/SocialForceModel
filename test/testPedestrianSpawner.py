f3ba06e97c540396b29f51b583d9976f8a707b1f
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-12 16:15:13 +0100

    Add import

--- test/testPedestrianSpawner.py
+++ test/testPedestrianSpawner.py
@@ -7,6 +7,7 @@ import unittest
 
 import matplotlib.pyplot as plt
 
+from worldParameters.worldDimensions import *
 from pedestrianParameters.pedestrianSettings import *
 from spawners.pedestrianSpawner import *
 
