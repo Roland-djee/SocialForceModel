93b61ef7accdff043da888bf93213bb1cb1c9a36
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-14 09:36:00 +0100

    Add pedestrian id.

--- pedestrianParameters/pedestrianType.py
+++ pedestrianParameters/pedestrianType.py
@@ -3,9 +3,10 @@
 # class pedestrianType:
 class pedestrianType(object):
     '''Defines the type of pedestrian and packages the coordinates'''
-    def __init__(self, pedType, coordinates, velocity, target):
+    def __init__(self, pedType, id, coordinates, velocity, target):
         self.type     = pedType
+        self.id       = 'Pedestrian' + str(id)   
         self.position = coordinates
         self.velocity = velocity
         self.target   = target
-        self.color    = 'blue'    
+        self.color    = 'blue' 
