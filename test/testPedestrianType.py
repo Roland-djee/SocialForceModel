390fd83b80467102e378268638d8927bb69002f0
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-14 09:39:30 +0100

    Add id test.

--- test/testPedestrianType.py
+++ test/testPedestrianType.py
@@ -19,12 +19,15 @@ class Test(unittest.TestCase):
 
     def testPedestrianTypeWithPedestrian(self):
         pedestrian = 'pedestrian'
+        id       = 10  
         position = np.array([2., 4., 6.]) 
         velocity = np.array([1., 3., 5.]) 
         target   = np.array([10., 15., 20.]) 
-        returnedPedestrian = pedestrianType(pedestrian, position, velocity, target)
+        returnedPedestrian = pedestrianType(pedestrian, id, position, velocity, target)
         errorMessage = "Wrong input value for pedestrian"
         self.assertEqual(returnedPedestrian.type, pedestrian, errorMessage)
+        errorMessage = "Wrong input value for id"
+        self.assertEqual(returnedPedestrian.id, 'Pedestrian'+str(id), errorMessage)
         errorMessage = "Wrong input value for position"
         self.assertEqual(returnedPedestrian.position.all(), position.all(), errorMessage)
         errorMessage = "Wrong input value for velocity"
