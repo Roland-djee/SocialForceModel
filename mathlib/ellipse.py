6f717f72369360b6d61e0de4b9a604ca5e123f92
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-15 13:33:14 +0100

    Add call to function for normalised desired direction vector.

--- mathlib/ellipse.py
+++ mathlib/ellipse.py
@@ -3,12 +3,14 @@
 from math import *
 import numpy as np
 
+from mathlib.vectors import normalisedDesiredDirection
+
 def ellipseSemiMinorAxis(dt, externalVelocity, position, externalPosition, externalTarget):
     ''' Returns the semiminor axis of the ellipse force field defined in the repulsive social force.
     Variable names follow [Helbing et al. PRE 51, 5 (1995)]'''
     rAlphaBeta = position - externalPosition
     b          = np.linalg.norm(rAlphaBeta)
-    eBeta      = (externalTarget - externalPosition) / np.linalg.norm(externalTarget - externalPosition)
+    eBeta      = normalisedDesiredDirection(externalTarget, externalPosition)
     sBeta      = np.linalg.norm(externalVelocity) * dt
     b          = b + np.linalg.norm(rAlphaBeta - sBeta * eBeta)
     return sqrt(b**2 - sBeta**2) * 0.5
