4cce357772fb353d1e4ce2ed555569873f04a4c7
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-15 13:37:03 +0100

    Simplify spawner

--- spawners/pedestrianSpawner.py
+++ spawners/pedestrianSpawner.py
@@ -4,19 +4,11 @@ import numpy as np
 
 from pedestrianParameters.pedestrianSettings import *
 from pedestrianParameters.standardPedestrian import *
-from worldParameters.worldDimensions import *
 
 def spawnRandomPedestrians():
     standardPedestrians = [None] * nbStandardPedestrians
     for i in range(nbStandardPedestrians):
-        position    = (np.random.rand(3) * 2. - 1.)
-        position[0] = position[0] * worldLength
-        position[1] = position[1] * worldWidth
-        velocity    = (np.random.rand(3) * 2. - 1.)
-        target      = (np.random.rand(3) * 2. - 1.)
-        target[0]   = target[0] * worldLength
-        target[1]   = target[1] * worldWidth
-        standardPedestrians[i] = standardPedestrian(standardPedestrian, i, position, velocity, target)
+        standardPedestrians[i] = standardPedestrian('standardPedestrian', i, 'random')
     return np.array(standardPedestrians)
 
 pedestrians = spawnRandomPedestrians()
