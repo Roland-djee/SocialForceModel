2d62b170bf659ff758279d4dd57b166a477e5445
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-15 13:38:48 +0100

    Set new parameters for test case.

--- behaviours/propagateParameters.py
+++ behaviours/propagateParameters.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 # set time-propagation parameters 
-dt     = 0.001   # Time spacing [s]
+dt     = 0.01   # Time spacing [s]
 tmax   = 10.     # Maximum propagation time [s]
 t      = 0.      # Initial timing [s]
 time   = np.linspace(0., tmax, int(tmax/dt) + 1) # time grid
