fbc5e5cae49872f83a1960e0cb9d16797b41eefb
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-15 13:35:43 +0100

    Simplify types and add initial conditions.

--- pedestrianParameters/pedestrianType.py
+++ pedestrianParameters/pedestrianType.py
@@ -3,10 +3,8 @@
 # class pedestrianType:
 class pedestrianType(object):
     '''Defines the type of pedestrian and packages the coordinates'''
-    def __init__(self, pedType, id, coordinates, velocity, target):
-        self.type     = pedType
-        self.id       = 'Pedestrian' + str(id)   
-        self.position = coordinates
-        self.velocity = velocity
-        self.target   = target
-        self.color    = 'blue' 
+    def __init__(self, pedType, id, initialConditions):
+        self.type              = pedType
+        self.id                = 'Pedestrian' + str(id)
+        self.initialConditions = initialConditions
+        self.color             = 'blue' 
