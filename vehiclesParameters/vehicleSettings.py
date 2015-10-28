7785608ec07f193ec4a87b5e43b30031af04b491
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-10-28 16:26:12 +0000

    Reset parameters

--- vehiclesParameters/vehicleSettings.py
+++ vehiclesParameters/vehicleSettings.py
@@ -5,11 +5,8 @@ standardCarLength = 4.5
 standardCarWidth  = 2.
 standardCarHeight = 0. 
 
-# set parameters for pedestrian-car repulsive forces from [Helbing & Molnar Phys. Rev. E 51, 4282 (1995)]
-v0        = 1.5         # Average desired speed [m/s]
-tauAlpha  = 0.5         # Relaxation time [s]
-V12       = 2.1         # Repulsive potential amplitude [m^2 s^-2]
-sigma     = 0.3         # Potential damping parameter [m]
-amplitude = V12 / sigma # Pedestrian-pedestrian repulsive force amplitude [m^-1 s^-2]
-
-
+# set parameters for car-car repulsive forces from [Anvari et al.]
+v0              = 13.9        # Average desired speed [m/s]
+tauAlpha        = 0.5         # Relaxation time [s]
+BAlphaU         = 0.3         # Potential damping parameter [m]
+amplitudeAlphaU = 10.         # Car-car repulsive force amplitude [m^-1 s^-2]
