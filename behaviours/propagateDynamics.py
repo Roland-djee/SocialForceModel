4c16487ff6548b234e538f1d2acfdc53db2edde1
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-11-12 15:30:10 +0000

    Remove useless imports.

--- behaviours/propagateDynamics.py
+++ behaviours/propagateDynamics.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
 
-from entities.pedestrians.pedestrianSettings import *
 from propagateParameters import *
-from spawners.buildingSpawner import *
-from spawners.pedestrianSpawner import *
-from spawners.vehicleSpawner import *
 from forces import *
-from matplotlib import animation
-from matplotlib import pyplot as plt
-
-from world.worldParameters import *
 
 def propagateInTime(t, currentEntity, pedestrians, cars, walls, buildings):
     '''Propagates the motion of a single pedestrian in time using adapted RK4 method'''
