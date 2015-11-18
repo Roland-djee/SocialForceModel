7f436ddaf1252019caad18c834bde244a279b28e
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-11-18 14:29:48 +0000

    Remove passing arguments to the superclass as zeros.

--- entities/pedestrians/standardPedestrian.py
+++ entities/pedestrians/standardPedestrian.py
@@ -4,7 +4,7 @@ from pedestrianSettings import *
 from pedestrianType import *
 
 class standardPedestrian(pedestrian):
-    def __init__(self, numberOfEntities, pedType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3)):
-        super(standardPedestrian, self).__init__(numberOfEntities, pedType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3))
+    def __init__(self, numberOfEntities, pedType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3), target2=np.zeros(3)):
+        super(standardPedestrian, self).__init__(numberOfEntities, pedType, id, initialConditions, position, target, velocity, target2)
         self.desiredVelocity = v0
         self.relaxationTime  = tauAlpha
--- entities/vehicles/standardCar.py
+++ entities/vehicles/standardCar.py
@@ -6,8 +6,8 @@ from vehicleType import *
 from vehicleSettings import *
 
 class standardCar(vehicle):
-    def __init__(self, numberOfEntities, vehType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3)):
-        super(standardCar, self).__init__(numberOfEntities, vehType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3))
+    def __init__(self, numberOfEntities, vehType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3), target2=np.zeros(3)):
+        super(standardCar, self).__init__(numberOfEntities, vehType, id, initialConditions, position, target, velocity, target2)
         self.desiredVelocity = v0
         self.relaxationTime  = tauAlpha
         self.length          = standardCarLength
