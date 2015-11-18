dfd42f355171730c0048dbab6380a33c20b10e12
Roland Guichard <roland.guichard@ts.catapult.org.uk>
2015-11-18 14:28:44 +0000

    Add possiblity to spawn entities in a defined area.

--- entities/vehicles/vehicleType.py
+++ entities/vehicles/vehicleType.py
@@ -7,7 +7,7 @@ from world.worldParameters import *
 # class vehicleType:
 class vehicle(entities):
     '''Defines the type of vehicle and packages the coordinates'''
-    def __init__(self, numberOfEntities, vehType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3)):
+    def __init__(self, numberOfEntities, vehType, id, initialConditions, position=np.zeros(3), target=np.zeros(3), velocity=np.zeros(3), target2=np.zeros(3)):
         super(vehicle, self).__init__(numberOfEntities)
         self.type              = vehType
         self.id                = id
@@ -28,9 +28,31 @@ class vehicle(entities):
             target[1]     = target[1] * worldWidth
             target[2]     = 0.
             self.target   = target 
+        if (initialConditions == 'area'):
+            randomPositionInArea = np.random.rand(3)
+            randomPositionInArea[0]   = randomPositionInArea[0] * (target[0] - position[0]) + position[0]
+            randomPositionInArea[1]   = randomPositionInArea[1] * (target[1] - position[1]) + position[1]
+            randomPositionInArea[2]   = 0.
+            self.position = randomPositionInArea
+            randomTargetInArea = np.random.rand(3)
+            randomTargetInArea[0]   = randomTargetInArea[0] * (target2[0] - velocity[0]) + velocity[0]
+            randomTargetInArea[1]   = randomTargetInArea[1] * (target2[1] - velocity[1]) + velocity[1]
+            randomTargetInArea[2]   = 0.
+            self.target = randomTargetInArea
+            self.velocity = np.array([0., 0., 0.])
         if (initialConditions == 'defined'):
             self.position = position
             self.velocity = velocity
             self.target   = target
         self.color             = 'green'
         self.movementStatus    = 'driving'
+        self.origin   = self.position
+        
+    def updatePosition(self, newPosition):
+        self.position = newPosition
+        
+    def updateOrigin(self, newOrigin):
+        self.origin = newOrigin
+        
+    def updateVelocity(self, newVelocity):
+        self.velocity = newVelocity
